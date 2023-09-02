63a52c23b6d4172755877b47788677c739e245e3
Andrew Ra <AndrewRa74@gmail.com>
2023-09-02 22:07:14 +0500

    Add Quick Sort metod

--- Algorithms.java
+++ Algorithms.java
@@ -1,17 +1,23 @@
+import java.util.Arrays;
 //Aditya Bhargava
 public class Algorithms {
     public static void main(String[] args) {
-        int[] ordered_massive    = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30};
-        int[] disordered_massive = {19,14,23,10,17,8,24,13,11,30,22,6,21,15,3,25,16,0,18,9,29,7,12,5,20,26,27,28,1,4,2};
+        int[] sorted_massive    = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30};
+        int[] unsorted_massive = {19,14,23,10,17,8,24,13,11,30,22,6,21,15,3,25,16,0,18,9,29,7,12,5,20,26,27,28,1,4,2};
+        int[] unsorted = {19,14,23,10,17,8,24,13,11,30,22,6,21,15,3,25,16,0,18,9,29,7,12,5,20,26,27,28,1,4,2};
         int lookingfor = 22;
-        SimpleSearch ss = new SimpleSearch(disordered_massive,lookingfor);
+        SimpleSearch ss = new SimpleSearch(unsorted_massive,lookingfor);
         System.out.println(ss);
-        BinarySearch bs = new BinarySearch(ordered_massive,lookingfor);
+        BinarySearch bs = new BinarySearch(sorted_massive,lookingfor);
         System.out.println(bs);
-        SortByChoice sbc_min = new SortByChoice(disordered_massive);
-        SortByChoice sbc_max = new SortByChoice(disordered_massive);
+        SortByChoice sbc_min = new SortByChoice(unsorted_massive);
+        SortByChoice sbc_max = new SortByChoice(unsorted_massive);
         System.out.println(sbc_min.to_String());
         System.out.println(sbc_max);
+        System.out.println(Arrays.toString(unsorted));
+        QuickSort qs = new QuickSort(unsorted);
+        qs._quickSort(unsorted,0,unsorted.length-1);
+        System.out.println(qs.toString(unsorted));
     }
 }
 //Простой (прямой) поиск произвольного массива (скорость работы О-Большое: O(n) оп.)
@@ -111,6 +117,45 @@ class SortByChoice{
         for (int j: massive){
             str += (j + " ");
         }
+        return str;
+    }
+}
+//Быстрая Сортировка (Quick Sort)
+class QuickSort {
+    private int[] massive;
+    QuickSort(int[] massive) {
+        this.massive = massive;
+    }
+    public void _quickSort(int[] array, int low, int high) {
+        if (array.length == 0) return; //Базовый случай
+        if (low >= high) return; //Базовый случай, делить больше нечего
+        //Найдем опорный элемент (например, средний)
+        int middle = low + (high - low) / 2;
+        int pivot = array[middle];
+        //Разделим массив на 2 части меньше опорного элемента и больше опорного элемента
+        int i = low;
+        int j = high;
+        while (i <= j) {
+            while (array[i] < pivot) i++;//Пока элементы слева меньше опорного, оставляем их на месте
+            while (array[j] > pivot) j--;//Пока элементы справа больше опорного, оставляем их на месте
+            if (i <= j) { //Найденные элементы меняем местами
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        //Пока левая граница не пришла направо, а правая налево
+        //рекурсивно вызываем _quickSort
+        if (j > low) _quickSort(array, low, j);
+        if (i < high) _quickSort(array, i, high);
+    }
+    public String toString(int[] array) {
+        String str = "[";
+        int i = 0;
+        for ( i=0; i<array.length-1;i++) str += array[i]+", ";
+        str += array[i]+"]";
         return str;
     }
 }
